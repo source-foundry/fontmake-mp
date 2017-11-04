f94a8fc1a3537b9cf74f2019df2c219520a4fb61
Chris Simpkins <git.simpkins@gmail.com>
2017-11-04 18:23:26 -0400

    refactor to main function

--- fmp.py
+++ fmp.py
@@ -17,29 +17,15 @@ from multiprocessing import Lock, Pool, cpu_count
 from fontmake.font_project import FontProject
 
 PROCESSES = 0
-BUILD_FILE_TYPE = ('ttf', 'otf')
+build_file_type = ('ttf', 'otf')
 
 lock = Lock()
 
 
-def build_fonts(ufo_path):
-    try:
-        fp = FontProject()
-        fp.run_from_ufos(ufo_path, output=BUILD_FILE_TYPE)
-    except Exception as e:
-        lock.acquire()
-        print(" ")
-        print("[ERROR] The fontmake compile for " + ufo_path + " failed with the following error"
-              ":" + os.linesep)
-        sys.stdout.flush()
-        traceback.print_exc()
-        print(str(e))
-        sys.stdout.flush()
-        lock.release()
+def main(argv):
+    processes = PROCESSES
 
-
-if __name__ == '__main__':
-    source_path_list = sys.argv[1:]
+    source_path_list = argv
 
     # Command line error handling
     if len(source_path_list) == 0:
@@ -60,21 +46,41 @@ if __name__ == '__main__':
         sys.exit(0)
     else:
         # if not defined by user, start by defining spawned processes as number of available cores
-        if PROCESSES == 0:
-            PROCESSES = cpu_count()
+        if processes == 0:
+            processes = cpu_count()
             print("[*] Detected " + str(cpu_count()) + " cores...")
         else:
-            print("[*] User request to spawn " + str(PROCESSES) + " processes for compilation...")
+            print("[*] User request to spawn " + str(processes) + " processes for compilation...")
 
         # if total cores available is greater than number of font compiles requested, limit to the latter number
-        if PROCESSES > len(source_path_list):
-            PROCESSES = len(source_path_list)
+        if processes > len(source_path_list):
+            processes = len(source_path_list)
             print("[*] Limiting spawned process number to the number of font compiles "
-                  "(" + str(PROCESSES) + ")...")
+                  "(" + str(processes) + ")...")
 
         print("[*] Output from fontmake will appear out of order below. This is expected...")
         print(" ")
         print(" ")
 
-        p = Pool(PROCESSES)
+        p = Pool(processes)
         p.map(build_fonts, source_path_list)
+
+
+def build_fonts(ufo_path):
+    try:
+        fp = FontProject()
+        fp.run_from_ufos(ufo_path, output=build_file_type)
+    except Exception as e:
+        lock.acquire()
+        print(" ")
+        print("[ERROR] The fontmake compile for " + ufo_path + " failed with the following error"
+              ":" + os.linesep)
+        sys.stdout.flush()
+        traceback.print_exc()
+        print(str(e))
+        sys.stdout.flush()
+        lock.release()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
