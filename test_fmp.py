ee4717fa76074ece727171a766e86d6c6a982a93
Chris Simpkins <git.simpkins@gmail.com>
2017-11-04 21:40:17 -0400

    added new tests

--- test_fmp.py
+++ test_fmp.py
@@ -1,10 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from fmp import build_fonts
+import os
+import shutil
+
+import fmp
+from fmp import main
 import pytest
 
 
-def test_pass():
-    pass
+def test_default_process_constant():
+    assert fmp.PROCESSES == 0
 
+
+def test_default_build_types():
+    assert 'ttf' in fmp.BUILD_FILE_TYPE
+    assert 'otf' in fmp.BUILD_FILE_TYPE
+
+
+def test_missing_args(capsys):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        main([])
+
+    out, err = capsys.readouterr()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
+
+
+def test_single_font_build(capsys):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        main(["tests/Hack-Regular.ufo"])
+
+    out, err = capsys.readouterr()
+    assert "Converting curves for Hack-Regular" in err
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+    assert os.path.exists("master_ttf") and os.path.isdir("master_ttf")
+    assert os.path.exists("master_otf") and os.path.isdir("master_otf")
+    assert os.path.exists("master_ttf/Hack-Regular.ttf")
+    assert os.path.exists("master_otf/Hack-Regular.otf")
+
+    if os.path.exists("master_ttf"):
+        shutil.rmtree("master_ttf")
+
+    if os.path.exists("master_otf"):
+        shutil.rmtree("master_otf")
+
+
+def test_multiple_font_build(capsys):
+    ufo_paths = ["tests/Hack-Regular.ufo", "tests/Hack-Italic.ufo", "tests/Hack-Bold.ufo", "tests/Hack-BoldItalic.ufo"]
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        main(ufo_paths)
+
+    out, err = capsys.readouterr()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+    assert os.path.exists("master_ttf") and os.path.isdir("master_ttf")
+    assert os.path.exists("master_otf") and os.path.isdir("master_otf")
+    assert os.path.exists("master_ttf/Hack-Regular.ttf")
+    assert os.path.exists("master_otf/Hack-Regular.otf")
+    assert os.path.exists("master_ttf/Hack-Italic.ttf")
+    assert os.path.exists("master_otf/Hack-Italic.otf")
+    assert os.path.exists("master_ttf/Hack-Bold.ttf")
+    assert os.path.exists("master_otf/Hack-Bold.otf")
+    assert os.path.exists("master_ttf/Hack-BoldItalic.ttf")
+    assert os.path.exists("master_otf/Hack-BoldItalic.otf")
+
+    if os.path.exists("master_ttf"):
+        shutil.rmtree("master_ttf")
+
+    if os.path.exists("master_otf"):
+        shutil.rmtree("master_otf")
