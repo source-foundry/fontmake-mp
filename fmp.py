dc46e9632ab442ba896fcd4f142c6b439e2a99bf
Chris Simpkins <git.simpkins@gmail.com>
2018-07-07 23:52:13 -0400

    [fmp.py] added ttf, otf options, parallelized ttf and otf builds

--- fmp.py
+++ fmp.py
@@ -11,15 +11,15 @@
 #   Source Repository: https://github.com/source-foundry/fontmake-mp
 # ===================================================================
 
-import sys
 import os
+import sys
+import timeit
 import traceback
 
 from multiprocessing import Lock, Pool, cpu_count
 from fontmake.font_project import FontProject
 
 PROCESSES = 0
-BUILD_FILE_TYPE = ("ttf", "otf")
 
 lock = Lock()
 
@@ -61,6 +61,9 @@ Fonts are compiled in the working directory on the directory path(s) master_otf 
 
 
 def main(argv):
+    if len(argv) == 0:
+        sys.stderr.write("[ERROR] Please include at least one UFO path in your command." + os.linesep)
+        sys.exit(1)
     # help, version, usage flag handling
     if argv[0] in ("-h", "--help"):
         print(HELP)
@@ -74,8 +77,27 @@ def main(argv):
 
     processes = PROCESSES
 
-    # TODO: handle --ttf and --otf options
-    source_path_list = argv
+    # define the build file types in the request
+    build_file_types = []
+    if "--ttf" in argv:
+        build_file_types.append("ttf")
+    if "--otf" in argv:
+        build_file_types.append("otf")
+
+    if len(build_file_types) == 0:
+        build_file_types.append("ttf")
+        build_file_types.append("otf")
+
+    # create source path list with single UFO path per build type
+    # definition in order to parallelize builds across otf/ttf types
+    source_path_list = []
+    for arg in argv:
+        if arg[0] == "-":
+            pass
+        else:
+            for build_type in build_file_types:
+                # create a tuple of (source file path, build type)
+                source_path_list.append((arg, build_type))
 
     # Command line error handling
     if len(source_path_list) == 0:
@@ -85,29 +107,27 @@ def main(argv):
         )
         sys.exit(1)
 
-    for source_path in source_path_list:
-        if len(source_path) < 5:  # not a proper *.ufo file path
+    for source_path_tuple in source_path_list:
+        if len(source_path_tuple[0]) < 5:  # not a proper *.ufo file path
             sys.stderr.write(
                 "[ERROR] '"
-                + source_path
+                + source_path_tuple[0]
                 + "' is not properly formatted as a path to a UFO source "
                 "directory" + os.linesep
             )
             sys.exit(1)
-        elif (
-            not source_path[-4:] == ".ufo"
-        ):  # does not end with .ufo directory extension
+        elif not source_path_tuple[0][-4:] == ".ufo":  # does not end with .ufo
             sys.stderr.write(
                 "[ERROR] '"
-                + source_path
+                + source_path_tuple[0]
                 + "' does not appear to be a UFO source directory"
                 + os.linesep
             )
             sys.exit(1)
-        elif not os.path.isdir(source_path):  # is not an existing directory path
+        elif not os.path.isdir(source_path_tuple[0]):  # is not an existing directory
             sys.stderr.write(
                 "[ERROR] '"
-                + source_path
+                + source_path_tuple[0]
                 + "' does not appear to be a valid path to a UFO source "
                 "directory" + os.linesep
             )
@@ -120,10 +140,23 @@ def main(argv):
     if len(source_path_list) == 1:
         # there is only one source compile necessary, skip spawning of processes and just build it
         print(
-            "[*] Single font compile requested. Concurrency is not necessary.  No additional processes spawned..."
+            "[*] Single font compile requested. No additional processes spawned..."
         )
         print(" ")
+
+        # begin compile
+        start_time = timeit.default_timer()
+
         build_fonts(source_path_list[0])
+
+        elapsed_time = timeit.default_timer() - start_time
+        elapsed_time_string = "{0:.2f}".format(elapsed_time)
+        print(os.linesep + os.linesep + "---")
+        print("Build complete in " + elapsed_time_string + " seconds!")
+        if "ttf" in build_file_types:
+            print("*.ttf fonts in master_ttf directory")
+        if "otf" in build_file_types:
+            print("*.otf fonts in master_otf directory")
         sys.exit(0)
     else:
         # if not defined by user, start by defining spawned processes as number of available cores
@@ -147,21 +180,36 @@ def main(argv):
         print(" ")
         print(" ")
 
+        # begin compile
+        start_time = timeit.default_timer()
+
         p = Pool(processes)
         p.map(build_fonts, source_path_list)
+
+        elapsed_time = timeit.default_timer() - start_time
+        elapsed_time_string = "{0:.2f}".format(elapsed_time)
+        print(os.linesep + os.linesep + "---")
+        print("Build complete in " + elapsed_time_string + " seconds!")
+        if "ttf" in build_file_types:
+            print("*.ttf fonts in master_ttf directory")
+        if "otf" in build_file_types:
+            print("*.otf fonts in master_otf directory")
         sys.exit(0)
 
 
-def build_fonts(ufo_path):
+def build_fonts(ufo_path_tuple):
+    """build_fonts uses fontmake to build fonts from UFO source files with
+       the definitions in the ufo_path_tuple that is defined as
+       (UFO source path, build file type)"""
     try:
         fp = FontProject()
-        fp.run_from_ufos(ufo_path, output=BUILD_FILE_TYPE)
+        fp.run_from_ufos(ufo_path_tuple[0], output=ufo_path_tuple[1])
     except Exception as e:
         lock.acquire()
         print(" ")
         print(
             "[ERROR] The fontmake compile for "
-            + ufo_path
+            + ufo_path_tuple[0]
             + " failed with the following error"
             ":" + os.linesep
         )
